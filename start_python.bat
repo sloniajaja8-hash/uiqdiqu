756e98809c681594b89430da1518bb9adebba2da
sloniajaja8-hash <sloniajaja8@gmail.com>
2025-10-18 13:59:56 +0200

    Delete start_python.bat

--- start_python.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting mObywatel 3.0 server with Python...
-echo.
-echo Server will run on http://localhost:8000
-echo Press Ctrl+C to stop the server
-echo.
-python -m http.server 8000
-pause
